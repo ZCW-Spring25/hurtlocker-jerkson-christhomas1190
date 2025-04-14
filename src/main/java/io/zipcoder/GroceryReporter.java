bcd66be5eb465cfade0771e058d50398bf4afd45
christhomas1190 <111439861+christhomas1190@users.noreply.github.com>
2025-04-14 15:11:19 -0400

    checkpoint

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -4,7 +4,12 @@ import io.zipcoder.utils.FileReader;
 import io.zipcoder.utils.Item;
 import io.zipcoder.utils.ItemParseException;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import static java.beans.NameGenerator.capitalize;
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -21,13 +26,35 @@ public class GroceryReporter {
 
     private void parseItems(){
         ItemParser itemParser=new ItemParser();
-        itemList=itemParser.parseItemList(this.originalFileText);
-    }
+        try {
+            itemList = itemParser.parseItemList(this.originalFileText);
+        } catch (ItemParseException e) {
+            e.printStackTrace();
+            itemList = new ArrayList<>();
+        }
+        }
     public List<Item> getItemList(){
         return itemList;
     }
+
     @Override
     public String toString() {
+        //creates map for item
+        Map<String, Integer> nameCounts=new HashMap<>();
+        //creates a nested array to store count of multiple of same item price
+        Map<String, Map<String,Integer>> priceCounts= new HashMap<>();
+        int errorCount=0;
+
+        for(Item item :itemList){
+            if(item==null||item.getName()==null||item.getPrice()==null) {
+                errorCount=0;
+            }
+            //capitalize comes from the JSL
+            String name=capitalize(item.getName());
+            //turns the double to a string
+            String price= String.valueOf(item.getPrice());
+
+        }
         return originalFileText;
     }
 }
